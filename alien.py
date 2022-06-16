0e40843c84ce085b5cd07fe72ea04e17a8990ba5
UserVert <rafael.anjos@vert.com.br>
2022-06-16 10:16:32 -0300

    :sparkles: Finalizando a classe do alienígena

--- alien.py
+++ alien.py
@@ -5,4 +5,13 @@ class Alien(Sprite):
     def __init__(self,game_config,screen):
         super(Alien, self).__init__()
         self.screen = screen
-        self.game_config = game_config 
+        self.game_config = game_config
+        self.image = pygame.image.load('images/alien.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = screen.rect.width
+        self.rect.y = self.rect.height
+        
+        self.x = float(self.rect.x)
+    
+    def blit_me(self):
+        self.screen.blit(self.image, self.rect)
